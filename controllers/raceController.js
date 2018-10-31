1e4ea2e9fb89dabda2bb47c5eb13a9fddaae18dc
Amanda Tennent <amandatennent@me.com>
2018-11-01 00:21:06 +1100

    Implemented selection functionality

--- controllers/raceController.js
+++ controllers/raceController.js
@@ -1,6 +1,26 @@
 const fetchJson = require('fetch-json');
 const helpers = require('../helpers');
 
-exports.displayRacePage = (req, res) => {
-	res.send('It works');
+exports.displayRacePage = async(req, res) => {
+	var json;
+	var selection;
+
+	function handleRaces(data) {
+		json = JSON.parse(data);
+	}
+
+	function handleSelection(data) {
+		var selectionData = JSON.parse(data);
+		selection = selectionData.selection;
+	}
+
+	await fetchJson.get(helpers.apiUrl + '/Races/' + req.params.racenumber)
+		.then(handleRaces)
+		.catch(console.error);
+
+	await fetchJson.get(helpers.apiUrl + '/Users/GetRaceSelection/' + req.user.id + '/' + json.RaceNumber)
+		.then(handleSelection)
+		.catch(console.error);
+
+	res.render('race', { title: json.RaceName, selection: selection, data: json });
 };
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,82 @@
 Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
-}
+	this.parentElement.removeChild(this);
+};
+
 NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
-}
+	for(var i = this.length - 1; i >= 0; i--) {
+		if(this[i] && this[i].parentElement) {
+			this[i].parentElement.removeChild(this[i]);
+		}
+	}
+};
 
 function closeFlashMessage() {
 	document.getElementsByClassName('flash').remove();
+}
+
+function makeSelection(userID, runnerID) {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://localhost:5000/api/Selections/MakeSelection/' + userID + '/' + runnerID, true);
+	request.onload = function () {
+		var data = JSON.parse(this.response);
+
+		if (data.UpdateStatus) {
+			var elements = document.querySelectorAll('.selected');
+			[].forEach.call(elements, function (element) {
+				element.classList.remove('selected');
+			});
+
+			document.getElementById(runnerID).classList.add('selected');
+
+			showSuccessMessage();
+		}
+		else
+			showErrorMessage();
+	};
+
+	request.send();
+}
+
+function showErrorMessage() {
+	document.getElementById('message').outerHTML = '';
+
+	var element = document.getElementById('custom_flash');
+	element.classList.remove('flash--success');
+	element.classList.add('flash--error');
+	element.style.display = 'flex';
+
+	var node = document.createElement('p');
+	node.setAttribute('id', 'message');
+
+	var textnode = document.createTextNode('Selection was not successful');
+	node.appendChild(textnode);
+
+	document.getElementById('custom_flash').insertBefore(node, document.getElementById('custom_flash').firstChild);
+
+	setTimeout(function()
+	{
+		document.getElementById('custom_flash').style.display = 'none';
+	}, 5000);
+}
+
+function showSuccessMessage() {
+	document.getElementById('message').outerHTML = '';
+
+	var element = document.getElementById('custom_flash');
+	element.classList.remove('flash--error');
+	element.classList.add('flash--success');
+	element.style.display = 'flex';
+
+	var node = document.createElement('p');
+	node.setAttribute('id', 'message');
+
+	var textnode = document.createTextNode('Selection was successful');
+	node.appendChild(textnode);
+
+	document.getElementById('custom_flash').insertBefore(node, document.getElementById('custom_flash').firstChild);
+
+	setTimeout(function()
+	{
+		document.getElementById('custom_flash').style.display = 'none';
+	}, 5000);
 }
